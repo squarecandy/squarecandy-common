dc2fb29b8a81a83246f1afb4dd009570a4709a5c
Eileen Mack <eileenmack@gmail.com>
2025-03-26 12:56:44 -0400

    fix: fix WP 6.7.1 image sizes auto bug

--- theme/inc/sqcdy-base-theme.php
+++ theme/inc/sqcdy-base-theme.php
@@ -251,4 +251,7 @@ if ( ! function_exists( 'squarecandy_staging_blogname' ) ) :
 	}
 endif;
 
+// disable poorly thought out WP sizes auto implementation
+add_filter( 'wp_img_tag_add_auto_sizes', '__return_false' );
+
 require_once 'sqcdy-base-theme-givewp.php';
